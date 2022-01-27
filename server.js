6f3bd1b544d46dd954ffeaf02fb776b326ff791a
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2022-01-27 00:24:32 -0800

    made changes to port (heroku default)

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-import * as path from 'path';
-
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
-import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
 import postRoutes from './routes/postRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
@@ -15,19 +13,12 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-mongoose.connect("mongodb+srv://oyeda:oyeda@cluster0.nfskq.mongodb.net/virt-pho-album-posts", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://oyeda:oyeda@cluster0.nfskq.mongodb.net/virt-pho-album-posts", { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
-});
+app.use(express.static('../frontend/build'));
 
 app.use("/", postRoutes);
 
-app.listen(3001, () => {
-    console.log('Server is currently running on port 3001')
+app.listen(PORT, () => {
+    console.log('Server is currently running on port' + PORT)
 })
