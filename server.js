8af4c3f44f86d14029d5a116d28aba91ee97eb72
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2022-01-28 15:17:09 -0800

    made changes to edit post

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import mongoose from 'mongoose';
 import postRoutes from './routes/postRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(express.json());
 
@@ -13,12 +14,13 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://oyeda:oyeda@cluster0.nfskq.mongodb.net/virt-pho-album-posts", { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://oyeda:oyeda@cluster0.nfskq.mongodb.net/virt-pho-album-posts", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://mockuser:mockuser@cluster0.nfskq.mongodb.net/photo-journal", { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(express.static('frontend/build'));
+// app.use(express.static('frontend/build'));
 
 app.use("/", postRoutes);
 
 app.listen(PORT, () => {
-    console.log('Server is currently running on port' + PORT)
+    console.log('Server is currently running on port ' + PORT)
 })
